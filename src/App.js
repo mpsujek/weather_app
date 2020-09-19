ca3de28dfe7709039fd4f8ba0fca7bd0d7d5c97d
mpsujek <mateusz.psujek@codilime.com>
2020-09-19 23:41:32 +0200

    Change project name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>Some app for learning</h1>
         <p>Just first step to start own app.</p>
         <p>Testing</p>
       </header>
